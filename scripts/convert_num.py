a35b5495d7bce22a009aecacb139733262f3bfca
aryanbaranwal001 <erenyeager108013@gmail.com>
2025-09-04 15:12:59 +0530

    updated conver_num

--- scripts/convert_num.py
+++ scripts/convert_num.py
@@ -7,15 +7,21 @@ INPUTS = [
 def main(args):
     if "hex" in args and args["hex"] is not None:
         value = int(args["hex"], 16)
+        return (
+            f"dec : {value}\n"
+            f"bin : {bin(value)[2:]}"
+        )
     elif "dec" in args and args["dec"] is not None:
         value = int(args["dec"])
+        return (
+            f"hex : {hex(value).upper().replace('X', 'x')}\n"
+            f"bin : {bin(value)[2:]}"
+        )
     elif "bin" in args and args["bin"] is not None:
         value = int(args["bin"], 2)
+        return (
+            f"dec : {value}\n"
+            f"hex : {hex(value).upper().replace('X', 'x')}"
+        )
     else:
         raise ValueError("Provide one of 'hex', 'dec', or 'bin' as input.")
-
-    return (
-        f"dec : {value}\n"
-        f"hex : {hex(value).upper().replace('X', 'x')}\n"
-        f"bin : {bin(value)[2:]}"
-    )
